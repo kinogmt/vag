677650c94113fce168f6038735e8f4d08b7ade42
Satoshi Kinoshita <kino@cloudian.com>
2017-11-21 10:57:07 +0900

    update

--- setup.sh
+++ setup.sh
@@ -7,18 +7,18 @@ IP4=`vagrant ssh v4 -- hostname -i|awk -e '{print $1}'`
 IP5=`vagrant ssh v5 -- hostname -i|awk -e '{print $1}'`
 IP6=`vagrant ssh v6 -- hostname -i|awk -e '{print $1}'`
 
-echo $IP1 s3-region1.mycloudianhyperstore.com
-echo $IP1 s3-admin.mycloudianhyperstore.com
-echo $IP1 cmc.mycloudianhyperstore.com
+echo $IP1  s3-region1.cloudian.com s3-region1.mycloudianhyperstore.com 
+echo $IP1  s3-admin.cloudian.com s3-admin.mycloudianhyperstore.com
+echo $IP1  cmc.cloudian.com cmc.mycloudianhyperstore.com
 
-echo $IP1 v1
-echo $IP2 v2
-echo $IP3 v3
-echo $IP4 v4
-echo $IP5 v5
-echo $IP6 v6
+echo $IP1 v1.ec2.internal v1
+echo $IP2 v1.ec2.internal v2
+echo $IP3 v1.ec2.internal v3
+echo $IP4 v1.ec2.internal v4
+echo $IP5 v1.ec2.internal v5
+echo $IP6 v1.ec2.internal v6
 
-echo
+echo ==================test.csv
 
 echo region1,v1,$IP1,DC1,RAC1
 echo region1,v2,$IP2,DC1,RAC1
@@ -26,3 +26,12 @@ echo region1,v3,$IP3,DC1,RAC1
 echo region1,v4,$IP4,DC1,RAC1
 echo region1,v5,$IP5,DC1,RAC1
 echo region1,v6,$IP6,DC1,RAC1
+
+echo ===============dnsmasq.conf
+
+echo address=/.s3-region1.cloudian.com/127.0.0.1
+echo address=/s3-region1.cloudian.com/127.0.0.1
+echo address=/.s3-website-region1.cloudian.com/127.0.0.1
+echo address=/cmc.cloudian.com/127.0.0.1
+echo address=/s3-admin.cloudian.com/127.0.0.1
+
