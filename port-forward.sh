4e0f9f05b0ec50ad540be3ca93a3cc29b81521f2
Satoshi Kinoshita <kino@cloudian.com>
2018-04-13 16:08:59 +0900

    port-forward

---
+++ port-forward.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ $# -eq 3 ]; then
+    if [ $3 != del ]; then
+        echo "usage: $0 <from-port> <to-ip:to-port> (del)"
+        exit 1
+    fi
+    sudo iptables -D PREROUTING -t nat -p tcp --dport $1 -j DNAT --to $2
+    exit 0
+fi
+
+if [ $# -eq 2 ]; then
+    sudo iptables -I PREROUTING -t nat -p tcp --dport $1 -j DNAT --to $2
+    exit 0
+fi
+
+echo "usage: $0 <from-port> <to-ip:to-port>"
+exit 2
+
