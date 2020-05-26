6cbbf78b01c70ada03a1b343ba8dee35f5208d6e
Satoshi Kinoshita <kino@cloudian.com>
2020-05-26 11:21:18 +0900

    update docker-compose

--- install.sh
+++ install.sh
@@ -91,7 +91,7 @@ usermod -a -G $DR $ID > /dev/null 2>&1
 usermod -a -G $DR vagrant > /dev/null 2>&1
 
 # --- docker compose ---
-curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
